6fa8034518e8c0f4c02c1f3a74a6533af9e5c477
Shreesht <Shreesht112@gmail.com>
2026-03-22 19:54:28 +0530

    state has been set up

---
+++ core/state.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, ConfigDict
+from typing import Any
+
+class AnalysisState(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    
+    file_path: str
+    raw_df: Any = None
+    clean_df: Any = None
+    stats_summary: dict = {}
+    charts: list = []
+    insights: dict = {}
+    errors: list = []
+    current_step: str = "start"
